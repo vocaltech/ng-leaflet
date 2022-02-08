8f5ef2abd98f716d1894217cb0bc9e1b06b155ca
vocaltech <it@vocaltech.fr>
2022-02-08 23:15:48 +0100

    implement AfterViewInit

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-map',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+  private map!: L.Map;
 
   constructor() { }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [43.604652, 1.444209], // Toulouse
+      zoom: 13
+    })
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+  
+    tiles.addTo(this.map);
+  }
+
+  // Angular lifecycle
   ngOnInit(): void {
 
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap()
   }
 }
